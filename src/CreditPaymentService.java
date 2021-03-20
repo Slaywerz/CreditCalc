d1fdb27b8ffbcb45cc193960b99b9484fb96a529
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-03-20 14:14:26 +0500

    Credit calculator finished

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class CreditPaymentService {
-    public double calculate(int creditAmount) {
-        return 0;
+    public int calculate(int creditAmount, int creditTerm) {
+        Scanner in = new Scanner(System.in);
+
+        int termInMonth = creditTerm * 12;
+        double interestRate = 9.99;
+        double ratePerMonth = interestRate / 100 / 12;
+        double x = Math.pow(1 + ratePerMonth, termInMonth); // variable to reducing formula.
+        int monthlyPayment = (int) (creditAmount * ((ratePerMonth * x) / (x - 1)));
+
+        return monthlyPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,24 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         Scanner in = new Scanner(System.in);
+        System.out.print("Введите желаемую сумму кредитования: ");
+        int creditAmount = in.nextInt();
+        while (100_000 > creditAmount || 5_000_000 < creditAmount) {
+            System.out.println("Сумма кредитования составляет от 100 000 до 5 000 000 рублей.");
+            System.out.print("Введите желаемую сумму кредитования: ");
+            creditAmount = in.nextInt();
+        }
 
-
-//        System.out.print("Введите желаемую сумму кредитования: ");
-//        int creditAmount = in.nextInt();
         int creditTerm;
-        do {
+        System.out.print("Введите срок желаемого кредитования: ");
+        creditTerm = in.nextInt();
+        while (1 > creditTerm || 3 < creditTerm) {
+            System.out.println("Срок кредитования может составлять от 1 года до 3 лет");
             System.out.print("Введите срок желаемого кредитования: ");
             creditTerm = in.nextInt();
         }
-        while (3 < creditTerm);
-        System.out.println(creditTerm);
-
-//        boolean b= 3 < creditTerm;
-//        if (b) {
-//            System.out.println("Срок кредитования составляет от 1 года до 3 лет.");
-        }
+        int monthlyPayment = service.calculate(creditAmount, creditTerm);
+        System.out.println("Ваш ежемесячный платеж будет составлять: " + monthlyPayment + " рублей.");
+//        System.out.println("Ваша процентная ставка: " + interestRate + " %.");
     }
+}
